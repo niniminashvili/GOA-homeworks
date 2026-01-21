9993f1893c9cd68d8ed4f9ddd6d994f3604a6cb4
niniminashvili <niniminashvili007@gmail.com>
2026-01-21 21:22:49 +0400

    first commitforward(50)

---
+++ day 0/lesson0.py
@@ -0,0 +1,94 @@
+print ("day 0 homework")
+from turtle import*
+#we want to paint a house
+
+#step 1:  draw a square
+shape("circle")
+speed(30)
+width(7)
+color("purple")
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)   
+
+forward(200)
+left(90) 
+
+forward(200)
+left(90) 
+#end of square
+
+#drawing a door
+
+forward(70)
+color("yellow")
+begin_fill()
+
+left(90)
+forward(120)   #height of the door
+right(90)
+forward(60)
+right(90)
+forward(120)
+
+end_fill()
+
+penup()
+goto(200, 200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+#drawing windows
+
+penup()
+goto(20, 130)
+pendown()
+left(120)
+
+color("blue")
+begin_fill()
+
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+
+end_fill()
+
+penup()
+goto(120, 130)
+setheading(90)
+pendown()
+right(90)
+
+color("blue")
+begin_fill()
+
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+forward(50)
+left(90)
+
+end_fill()
+
+
+
+exitonclick()

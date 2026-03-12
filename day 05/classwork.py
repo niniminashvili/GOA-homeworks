059cb82d782d16a52e81e100b80f011061877e50
niniminashvili <niniminashvili007@gmail.com>
2026-03-12 19:04:50 +0400

    first commit

---
+++ day 05/classwork.py
@@ -0,0 +1,20 @@
+# 1) debugging ნიშნავს პროგრამაში შეცდომების პოვნას და გასწორებას
+
+# 2)
+first_name = "Nini"
+
+# 3) შეიქმნა 2 ცვლადი. name , Name
+
+# 4) ეს კოდი გამოიტანს error -ს 
+
+# 5) 
+name = "Lika"
+print(name)
+
+first_user = "Luka"
+second_user = "Giorgi" 
+print(first_user)
+
+name1 = "levani"
+name2 = "demetre"
+print(name1 + name2)

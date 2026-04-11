e6c49e7cca65ba71f8deaa29c82949619e56c8ff
Nini <nini@MacBook-Air-Nini.local>
2026-04-11 14:01:08 +0400

    first commit

---
+++ day 09/homework.py
@@ -0,0 +1,21 @@
+# 1) 
+num = input("სეიყვანე რიცხვი: ")
+num = int(num)
+print("ორმაგი მნიშვნელობა: ", num * 2)
+
+# 2)
+num1 = input("შეიყვანე პირველი რიცხვი: ")
+num2 = input("შეიყვანე მეორე რიცხვი: ")
+
+num1 = float(num1)
+num2 = float(num2)
+
+print("ჯამი:", num1 + num2)
+
+# 3)
+data = input("შეიყვანე რამე: ")
+
+int(data)
+print("ეს არის int")
+
+print("ეს არ არის int")

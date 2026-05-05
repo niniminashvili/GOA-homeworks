acf29dbd66f87a34b39f2b61d52df7e62d9590b7
Nini <nini@MacBook-Air-Nini.local>
2026-05-05 23:00:00 +0400

    first commit

---
+++ day 11/classwork.py
@@ -0,0 +1,17 @@
+# 1) and აბრუნებს true-ს მხოლოდ მაშინ როცა ორივე პირობა true არის
+# or აბრუნებს true-ს მაშინაც კი თუ მხოლოდ ერთი პირობაა true
+
+# 2) 
+print(5 > 3)
+print(10 <= 7)
+
+print(True and False) #false
+print(True or False) #true
+
+# 3) ეს კოდი გამოიტანს True-ს
+
+# 4) ეს კოდი გამოიტანს True-ს
+
+# 5)
+random = 2*3
+print(random > 5 and False)

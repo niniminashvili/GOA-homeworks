5226288f88515d3c881084d37d569b4eda514746
Nini <nini@MacBook-Air-Nini.local>
2026-05-18 20:25:55 +0400

    first commit

---
+++ day 13/classwork.py
@@ -0,0 +1,20 @@
+# 1)
+for i in range(1, 26):
+    print(i)
+
+# 2)
+for i in range(2, 47, 2):
+    print(i)
+
+# 3)
+for i in range(100):
+    print("nini")
+
+# 4)
+num1 = int(input("შეიყვანე პირველი რიცხვი: "))
+num2 = int(input("შეიყვანე მეორე რიცხვი: "))
+
+for i in range(num1, num2):
+    print(i)
+
+print(num2)
---
+++ day 13/homework.py
@@ -0,0 +1,17 @@
+# 1)
+for i in range(1, 21):
+    print(i)
+
+# 2)
+for i in range(2, 31, 2):
+    print(i)
+
+# 3)
+total = 0
+for i in range(1, 11):
+    total+=i
+print(total)
+
+# 4)
+for i in range(15):
+    print("*")
